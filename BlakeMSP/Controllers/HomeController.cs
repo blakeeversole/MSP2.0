0d73d31b1289f1a4e62b8613a6dde9b3740e21f0
blakeeversole <43444538+blakeeversole@users.noreply.github.com>
2018-09-28 15:39:47 -0400

    Changes

---
+++ BlakeMSP/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlakeMSP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlakeMSP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlakeMSP.MvcApplication" Language="C#" %>
